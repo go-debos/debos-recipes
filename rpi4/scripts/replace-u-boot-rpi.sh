82e3ddb15385d035aeb1cab6e6a7ab0949c3725d
Andreas Henriksson <andreas@fatal.se>
2020-08-05 16:49:52 +0200

    rpi4: Drop replace-u-boot-rpi.sh hack
    u-boot 2020.07 (with rpi4 target enabled) has now migrated to
    testing/bullseye, so we don't need to pull the u-boot version
    from unstable anymore and can drop this hack.

--- rpi4/scripts/replace-u-boot-rpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-#wget https://people.debian.org/~ah/u-boot-rpi_2020.04+dfsg-2+rpi4_arm64.deb
-#dpkg -i u-boot-rpi_2020.04+dfsg-2+rpi4_arm64.deb
-#rm u-boot-rpi_2020.04+dfsg-2+rpi4_arm64.deb
-wget https://snapshot.debian.org/archive/debian/20200709T084659Z/pool/main/u/u-boot/u-boot-rpi_2020.07%2Bdfsg-1_arm64.deb
-dpkg -i u-boot-rpi_2020.07+dfsg-1_arm64.deb
-rm u-boot-rpi_2020.07+dfsg-1_arm64.deb
