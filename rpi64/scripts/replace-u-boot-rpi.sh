1a490b3de55f8e1d4585eae6fe294daba365e031
Denis Pynkin <denis.pynkin@collabora.com>
2020-09-12 22:09:34 +0300

    rpi64: removed u-boot replacement script
    Debian already have RPi3 and RPi4 support in U-Boot hence do not
    need the replacement script anymore.
    
    Signed-off-by: Denis Pynkin <denis.pynkin@collabora.com>

--- rpi64/scripts/replace-u-boot-rpi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-wget http://ftp.us.debian.org/debian/pool/main/u/u-boot/u-boot-rpi_2018.09+dfsg-1_arm64.deb
-dpkg -i u-boot-rpi_2018.09+dfsg-1_arm64.deb
-rm u-boot-rpi_2018.09+dfsg-1_arm64.deb
